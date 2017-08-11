d42d17abd8c5c69dad6615a38892dcbb8a81169d
laxxy <19765328>
2017-08-11 13:52:08 +0300

    init

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Setup
+### Prerequisites
+- Python [3.5.2 or higher](https://www.python.org/downloads/)
+- [Virtualenvwrapper](https://virtualenvwrapper.readthedocs.io/en/latest/)
+- cloned [the repo]
+
+### Setting up the project
+- create new virtual env `mkvirtualenv -p <path/to/python3> fctests`,
+you could use any other name instead of `fctests`
+
+- activate the env by running `workon fctests`
+- run `python setup.py install`
+
+#Run locally
+
+
+# Run tests
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+def get_requirements():
+    """Reads the installation requirements from requirements.pip"""
+    with open("test-requirements.pip") as f:
+        lines = f.read().split("\n")
+        lines_without_comments = [line for line in lines if not line.startswith('#') and not line == '']
+        return lines_without_comments
+
+setup(name='selenium_t',
+      version='0.0.1',
+      description='lantern_dno',
+      author='',
+      author_email='',
+      url='',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      extras_require={},
+      install_requires=get_requirements(),
+      )
